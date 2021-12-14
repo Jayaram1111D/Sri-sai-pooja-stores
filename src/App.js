f9ec00cb618298ed02efb776051eb290f313cae2
unknown <jayarammahesh27@gmail.com>
2021-12-14 11:57:55 +0530

    srisai

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer" ;
+import {Container} from 'react-bootstrap';
+import './bootstrap.min.css'
+import HomeScreen from "./screens/HomeScreen";
+
+
+function App  ()  {
+  return (
+    <>
+    <Header/>
+    <main>
+    <Container>
+    <HomeScreen/>
+
+    </Container>
+    </main>
+    <Footer/>
+   
+      
+    </>
+  );
+}
+
+export default App
